22947c9ac80517078bd2b99872f991dbc4fbeb73
Sergio Revilla <replymusic@gmail.com>
2017-09-05 11:09:53 +0200

    About Triangle2

--- koans/triangle.py
+++ koans/triangle.py
@@ -20,6 +20,7 @@
 EQUILATERAL_SIDES = 1
 ISOSCELES_SIDES = 2
 SCALENE_SIDES = 3
+MINIMUM_SIDE_SIZE = 0
 
 RULES = {
   EQUILATERAL_SIDES: 'equilateral',
@@ -28,11 +29,28 @@ RULES = {
 }
 
 def triangle(a, b, c):
-    sides = [a, b, c]
+    sides = check_sides_length(a, b, c)
+    check_sides_sum(a, b, c)
+    return unique(sides)
+
+def unique(sides):
     unique_sides = len(set(sides))
 
     return RULES[unique_sides]
 
+def check_sides_length(a, b, c):
+  sides = [a, b, c]
+
+  for side in sides:
+    if side <= MINIMUM_SIDE_SIZE:
+      raise TriangleError('A side must be a positive integer')
+
+  return sides
 # Error class used in part 2.  No need to change this code.
+
+def check_sides_sum(a, b, c):
+  if ((a + b) < c or (a + c) < b or (b + c) < a):
+    raise TriangleError('Illegal sides sum')
+
 class TriangleError(Exception):
     pass
